742c4ef953fb6f4d401e76c5f5c22e67bde2bae9
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2023-02-18 11:37:59 +0300

    Update complete-topic.service.ts

--- src/complete-topic/complete-topic.service.ts
+++ src/complete-topic/complete-topic.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CompleteTopic } from './schema/complete-topic.schema';
 
 @Injectable()
-export class CompleteTopicService {}
+export class CompleteTopicService {
+    constructor(
+        @InjectModel('CompleteTopic') private readonly completeTopicModel: Model<CompleteTopic>
+    ){}
+
+    
+
+}
